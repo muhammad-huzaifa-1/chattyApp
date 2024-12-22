a7deadab63fd187cdc4874a93a539abbbb125806
muhammad-huzaifa-1 <huzaifaktk0905@gmail.com>
2024-12-22 18:25:52 +0500

    Create index.js

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+import express from "express"
+import dotenv from "dotenv";
+import { dbConnection } from "./src/lib/db.js";
+import authRoutes from "./src/routes/auth.route.js";
+import messageRoutes from "./src/routes/message.route.js";
+import cookieparser from "cookie-parser";
+import cors from "cors";
+import {app, server} from "./src/lib/socket.js";
+
+
+dotenv.config();
+dbConnection
+const PORT = process.env.PORT || 5000 ;
+
+app.use(express.json({
+    limit:"100mb"
+}));
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}));
+app.use(cookieparser());
+
+app.get('/',(req,res)=>{
+    res.send("API Working");
+})
+app.use('/api/auth',authRoutes)
+
+app.use('/api/messages', messageRoutes)
+
+server.listen(PORT,()=>{
+    console.log("Server is running on port" +" "+ PORT);
+});
