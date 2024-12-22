6721d3f7d0427050237011577179d136d218a7f0
muhammad-huzaifa-1 <huzaifaktk0905@gmail.com>
2024-12-22 18:32:10 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(express.json({
     limit:"100mb"
 }));
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://chatty-app-frontend.vercel.app",
     credentials:true
 }));
 app.use(cookieparser());
