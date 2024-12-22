873828a63801e83f5fee74f304f78982a0028263
muhammad-huzaifa-1 <huzaifaktk0905@gmail.com>
2024-12-23 01:48:31 +0500

    Update socket.js

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,7 +7,9 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors:{
-        origin:["http://localhost:5173"]
+     credentials:true,
+     origin:"https://chatty-app-frontend.vercel.app",
+     methods: ["GET", "POST", "PUT","DELETE"],
     }
 })
 
